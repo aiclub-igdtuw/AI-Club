1476f4853298a07793d768a4237bd7e30a881881
Tanya <tanyadiit7@gmail.com>
2022-05-28 13:58:40 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 4004
+
+//view engine setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/aboutus', (req, res) => {
+    res.render('aboutus');
+})
+
+app.get('/team', (req, res) => {
+    res.render('team');
+})
+
+app.get('/events', (req, res) => {
+    res.render('events');
+})
+
+// app.get('/tad', (req, res) => {
+//     res.render('tad');
+// })
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
