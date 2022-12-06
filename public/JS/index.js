ab7c3724938c38133f0a39399b9308e7d3f018ac
Tanya <tanyadiit7@gmail.com>
2022-12-06 20:23:45 +0530

    new changes

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,13 +1,115 @@
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('nav')
-    if(this.scrollY >= 200) nav.classList.add('nav__scroll'); else nav.classList.remove('nav__scroll')
+    if (this.scrollY >= 200) nav.classList.add('nav__scroll'); else nav.classList.remove('nav__scroll')
 }
 window.addEventListener('scroll', scrollHeader)
-function switch_theme()
-{
-    document.querySelector(".theme_switch .fa-solid").addEventListener("click", function(){
+function switch_theme() {
+    document.querySelector(".theme_switch .fa-solid").addEventListener("click", function () {
         document.querySelector(".theme_switch .fa-solid").classList.toggle("fa-sun");
         document.querySelector("body").classList.toggle("light");
     })
 }
 switch_theme()
+
+jQuery('.card-slider').slick({
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+let cards = document.querySelectorAll('.card');
+let card;
+let modal = document.querySelector('.modal');
+let closeButton = document.querySelector('.modal__close-button');
+let page = document.querySelector('.page');
+const cardBorderRadius = 20;
+const openingDuration = 600; //ms
+const closingDuration = 600; //ms
+const timingFunction = 'cubic-bezier(.76,.01,.65,1.38)';
+
+var Scrollbar = window.Scrollbar;
+Scrollbar.init(document.querySelector('.modal__scroll-area'));
+
+
+function flipAnimation(first, last, options) {
+    let firstRect = first.getBoundingClientRect();
+    let lastRect = last.getBoundingClientRect();
+
+    let deltas = {
+        top: firstRect.top - lastRect.top,
+        left: firstRect.left - lastRect.left,
+        width: firstRect.width / lastRect.width,
+        height: firstRect.height / lastRect.height
+    };
+
+    return last.animate([{
+        transformOrigin: 'top left',
+        borderRadius: `
+      ${cardBorderRadius / deltas.width}px / ${cardBorderRadius / deltas.height}px`,
+        transform: `
+        translate(${deltas.left}px, ${deltas.top}px) 
+        scale(${deltas.width}, ${deltas.height})
+      `
+    }, {
+        transformOrigin: 'top left',
+        transform: 'none',
+        borderRadius: `${cardBorderRadius}px`
+    }], options);
+}
+
+cards.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        jQuery('.card-slider').slick('slickPause');
+        card = e.currentTarget;
+        page.dataset.modalState = 'opening';
+        card.classList.add('card--opened');
+        card.style.opacity = 0;
+        document.querySelector('body').classList.add('no-scroll');
+
+        let animation = flipAnimation(card, modal, {
+            duration: openingDuration,
+            easing: timingFunction,
+            fill: 'both'
+        });
+
+        animation.onfinish = () => {
+            page.dataset.modalState = 'open';
+            animation.cancel();
+        };
+    });
+});
+
+closeButton.addEventListener('click', (e) => {
+    page.dataset.modalState = 'closing';
+    document.querySelector('body').classList.remove('no-scroll');
+
+    let animation = flipAnimation(card, modal, {
+        duration: closingDuration,
+        easing: timingFunction,
+        direction: 'reverse',
+        fill: 'both'
+    });
+
+    animation.onfinish = () => {
+        page.dataset.modalState = 'closed';
+        card.style.opacity = 1;
+        card.classList.remove('card--opened');
+        jQuery('.card-slider').slick('slickPlay');
+        animation.cancel();
+    };
+});
